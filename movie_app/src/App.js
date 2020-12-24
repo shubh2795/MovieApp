756329836b1c137c3c367d1411833d198d7799d9
Shubham Swami <shubhamswami2795@gmail.com>
2020-12-24 16:42:05 -0700

    got data from ombd api

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MovieList from "./components/MovieList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 const App = () => {
-  return <div>hello!</div>;
+  const [movies, setMovies] = useState([
+    {
+      Title: "Star Wars: Episode IV - A New Hope",
+      Year: "1977",
+      imdbID: "tt0076759",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode V - The Empire Strikes Back",
+      Year: "1980",
+      imdbID: "tt0080684",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VI - Return of the Jedi",
+      Year: "1983",
+      imdbID: "tt0086190",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VII - The Force Awakens",
+      Year: "2015",
+      imdbID: "tt2488496",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode I - The Phantom Menace",
+      Year: "1999",
+      imdbID: "tt0120915",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode III - Revenge of the Sith",
+      Year: "2005",
+      imdbID: "tt0121766",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg",
+    },
+  ]);
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?i=tt3896198&apikey=a737d8d6";
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    console.log(responseJson);
+  };
+
+  useEffect(() => {
+    getMovieRequest();
+  }, []);
+
+  return (
+    <div className="container-fluid movie_app">
+      <div className="row">
+        <MovieList movies={movies}></MovieList>
+      </div>
+    </div>
+  );
 };
 
 export default App;
